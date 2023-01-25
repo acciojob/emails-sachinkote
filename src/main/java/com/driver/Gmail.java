9199446b74d57bbf696ac8adc8199f1733e402b6
sachinkote <65725161+sachinkote@users.noreply.github.com>
2023-01-25 15:45:04 +0530

    Update Gmail.java

--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -56,7 +56,7 @@ public class Gmail extends Email {
         	Inbox.remove(m);
         	Trash.add(m);
         }
-       
+       System.out.println(message);
     }
 
     public String findLatestMessage(){
